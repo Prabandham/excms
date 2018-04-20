585911ca0925b436aafcfd6d992aee323f56b428
Srinidhi Prabandham <srinidhi.prabandham@icicletech.com>
2018-04-20 15:57:13 +0530

    [Sites] added new and create actions and pages

--- lib/ex_cms_web/controllers/sites_controller.ex
+++ lib/ex_cms_web/controllers/sites_controller.ex
@@ -7,14 +7,30 @@ defmodule ExCmsWeb.SitesController do
     render(conn, "index.html", sites: sites)
   end
 
+  def new(conn, _params) do
+    changeset = ExCms.Sites.change_site(%ExCms.Sites.Site{})
+    pages = [%ExCms.Sites.Page{}]
+    render(conn, "new.html", changeset: changeset, pages: pages)
+  end
+
+  def create(conn, %{"site" => sites_params}) do
+    case ExCms.Sites.create_site(sites_params) do
+      {:ok, site} ->
+        conn
+        |> put_flash(:info, "Site created successfully")
+        |> redirect(to: sites_path(conn, :index))
+
+      {:error, changeset} ->
+        pages = [%ExCms.Sites.Page{}]
+        conn
+        |> put_flash(:error, "Please correct the errors below")
+        |> render("new.html", changeset: changeset, pages: pages)
+    end
+  end
+
   def show(conn, %{"id" => id}) do
     site = Sites.get_site!(id)
     render(conn, "show.html", site: site)
-  end
-
-  def new(conn, _params) do
-    changeset = ExCms.Sites.change_site(%ExCms.Sites.Site{})
-    render(conn, "new.html", changeset: changeset)
   end
 
   def edit(conn, %{"id" => id}) do
