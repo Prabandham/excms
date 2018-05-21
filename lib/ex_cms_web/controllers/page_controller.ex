1747e96390014451cdfab23dd37898d95267924a
Srinidhi Prabandham <srinidhi.prabandham@icicletech.com>
2018-05-21 16:51:48 +0530

    Handling www

--- lib/ex_cms_web/controllers/page_controller.ex
+++ lib/ex_cms_web/controllers/page_controller.ex
@@ -4,20 +4,32 @@ defmodule ExCmsWeb.PageController do
   plug(:put_layout, false)
 
   def index(conn, %{"page" => page}) do
-    case ExCms.Utils.PageCache.get_site_from_cache(conn.host) do
+    host = if(String.contains?(conn.host, "wwww")) do
+      conn.host
+    else
+      [h | t] = String.split(".")
+      Enum.join(t, ".")
+    end
+    case ExCms.Utils.PageCache.get_site_from_cache(host) do
       {:error, %{}} -> render(conn, "no_site.html")
       {:ok, site} ->
-        content = ExCms.Utils.PageCache.get_page_from_cache(conn.host, page)
+        content = ExCms.Utils.PageCache.get_page_from_cache(host, page)
         render(conn, "index.html", content: content)
     end
   end
 
   # This will always be the root path only i.e "/"
   def index(conn, _params) do
-    case ExCms.Utils.PageCache.get_site_from_cache(conn.host) do
+    host = if(String.contains?(conn.host, "wwww")) do
+      conn.host
+    else
+      [h | t] = String.split(".")
+      Enum.join(t, ".")
+    end
+    case ExCms.Utils.PageCache.get_site_from_cache(host) do
       {:error, %{}} -> render(conn, "no_site.html")
       {:ok, site} ->
-        content = ExCms.Utils.PageCache.get_page_from_cache(conn.host, site.root_page)
+        content = ExCms.Utils.PageCache.get_page_from_cache(host, site.root_page)
         render(conn, "index.html", content: content)
     end
   end
